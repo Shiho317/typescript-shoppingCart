cdf53949ec0253ba3119f99b88e776588dc0ebe4
Shiho Nagano <shiho@nagano.com>
2022-01-20 10:55:14 -0800

    added styled button

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 import Grid from '@mui/material/Grid';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Badge from '@mui/material/Badge';
-import { Wrapper } from './App.style';
+import { Wrapper, StyledButton } from './App.style';
 import Item from './Item/Item';
 
 export type CartItemType = {
@@ -24,17 +24,29 @@ const getProducts = async (): Promise<CartItemType[]> =>
 
 const App = () => {
 
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => 
+  items.reduce((acc: number, items) => acc + items.amount, 0);
 
-  const handleAddToCart = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
 
   if(isLoading) return <LinearProgress/>;
   if(error) return <div>Something gone wrong...</div>
 
   return (
     <Wrapper>
+      <Drawer anchor='right' open={isCartOpen} onClose={() => setIsCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+      <StyledButton onClick={() => setIsCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon/>
+        </Badge>
+      </StyledButton>
       <Grid container spacing={3}>
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4}>
